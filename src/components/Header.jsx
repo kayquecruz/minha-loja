374f989204c7b58079b3e2dfb94ba0dc809d32c5
kayque cruz <cruzkayque442@gmail.com>
2024-03-16 01:41:14 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css';
+
+export default function Header() {
+  return (
+    <header>
+<h1>street hype</h1>
+      <div class="outros">
+        <ul>
+          <li><a href="#">Início</a></li>
+          <li><a href="#"> produtos</a></li>
+          <li><a href="#"> Contato</a></li>
+        </ul>
+      </div>
+    </header>
+  )
+}
